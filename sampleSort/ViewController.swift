a9038ce96a3f2e793c9b8fc41deed2e544191fa6
ei17ringo <ei17ringo@gmail.com>
2016-11-17 17:59:17 +0800

    Initial Commit

---
+++ sampleSort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleSort
+//
+//  Created by Eriko Ichinohe on 2016/11/17.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
